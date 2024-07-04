1d021afd7f5889410dfb4999fcb0e08909c04707
Vinod Jaggavarapu <jaggavarapu.sa@northeastern.edu>
2024-07-04 14:20:00 -0400

    JSX and React components

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "child" }, [
-    React.createElement("h1", { key: "h1c" }, "Im h1 tag"),
-    React.createElement("h2", { key: "h2c" }, "Im h2"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "h1c2" }, "Im h1 tag child2"),
-    React.createElement("h2", { key: "h2ce" }, "Im h2 tag child2"),
-  ]),
-]);
+const heading = React.createElement("h1", { id: "heading" }, "Hello React");
 
+// const jsxHeading = <h1 id="heading">Hello React from JSX</h1>;
+
+const Title = () => <h1 className="title">Component Composition</h1>;
+const HeadingComponent = () => {
+  return (
+    <>
+      <Title />
+      <h1 id="heading">Hello React from JSX functional component</h1>
+    </>
+  );
+};
+// console.log(jsxHeading);
+console.log(HeadingComponent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
